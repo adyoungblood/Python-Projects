b0b7d95deeb8c410a0e1613760038e654bc35c2d
arghalexander <arghalexander3000@gmail.com>
2017-06-01 13:58:35 -0700

    Check saved

--- bs4/youtubevideofetch/ytdownloader.py
+++ bs4/youtubevideofetch/ytdownloader.py
@@ -19,10 +19,14 @@ with open(savedfile, 'a', encoding='utf-8') as s:
     for link in output:
         s.write(link + " ")
 
-with open(savedfile, 'r', encoding='utf-8') as v:
+with open(htmlfile, 'r', encoding='utf-8') as v:
     td = v.read()
     td = td.split()
     print(td)
+    
+with open(savedfile, 'r', encoding='utf-8') as v:
+    td = (td - v.read())
+  
 
 """
 for video in td:
